fdf2ac30eb923bc5ee6d303b66ebc63d9f83da15
joi1191_Ow7 <031902641@fzu.edu.cn>
2021-09-19 17:30:23 +0800

    完成识别注释并删除的功能

---
+++ src/com/joi/TrimNote.java
@@ -0,0 +1,17 @@
+package com.joi;
+
+public class TrimNote {
+    public StringBuilder trimNote (StringBuilder string) {
+        String noteBlockStart = "/*";
+        String noteBlockEnd = "*/";
+        
+        while(string.indexOf("/*") != -1) {
+            int startIndex = string.indexOf(noteBlockStart);
+            int endIndex = string.indexOf(noteBlockEnd, startIndex + 1);
+
+            if(startIndex < 0 || endIndex < 0) break;
+            string = string.replace(startIndex, endIndex+2,"");
+        }
+        return string;
+    }
+}
